cd73f0e4f627cb7b6b684a0aed29808485fd43d7
Igor003 <igorio6ka@gmail.com>
2016-12-03 22:59:07 +0200

    доделал option

--- content.php
+++ content.php
@@ -20,7 +20,8 @@
                 success : function(result) {
                     // you can see the result from the console
                     // tab of the developer tools
-                   alert(result.calibrarea_sus  + result.calibrarea_jos);
+					$(".result").css("dispaly","none");
+					$(".result").append("<div>Calibrarea sus:" + result.calibrarea_sus + " \t\n Calibrarea jos:" + result.calibrarea_jos+"</div>")
                 },
                 error: function(xhr, resp, text) {
                     console.log(xhr, resp, text);
@@ -33,45 +34,20 @@
 	<script src="jquery.paulund_modal_box.js"></script>
 </head>
 <body>
-	 <a href="list.php"><button class="btn btn-success">Таблица пользователей</button></a>
-	 <form class="form" action="content.php" method="post">
-	 	<label for="terminal">Terminal</label>
-	 	<input type="text" name="terminal">
-	 	<label for="terminal">Miniaplicator</label>
-	 	<input type="text" name="mini">
-	 	<label for="terminal">Nr Presetei</label>
-	 	<input type="text" name="preseta">
-	 	<label for="terminal">Sez</label>
-	 	<input type="text" name="sez">
-<!--	 	<button class=" btn btn-primary" type="submit" name="submit">Отправить</button>-->
-	 	<button type="submit" class=" btn btn-primary ">Отправить</button>
-
-<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-  <div class="modal-dialog modal-sm" role="document">
-    <div class="modal-content">
-        <h2>Calirarea sus:<?php echo $a['calibrarea_sus']?></h2><br>
-	    <h2>Calirarea jos:<?php echo $a['calibrarea_jos']?></h2>
+	<a href="list.php"><button class="btn btn-success">Таблица пользователей</button></a>
+	<form class="form" action="content.php" method="post">
+		<label for="terminal">Terminal</label>
+		<input type="text" name="terminal">
+		<label for="terminal">Miniaplicator</label>
+		<input type="text" name="mini">
+		<label for="terminal">Nr Presetei</label>
+		<input type="text" name="preseta">
+		<label for="terminal">Sez</label>
+		<input type="text" name="sez">
+		<button type="submit" class=" btn btn-primary ">Отправить</button>
+	</form>
+    <div class="result ">
+    	
     </div>
-  </div>
-</div>
-	 
-	 </form>
-<!--
-	 <div class="info">
-	 	<h2>Calirarea sus:<?php echo $a['calibrarea_sus']?></h2><br>
-	    <h2>Calirarea jos:<?php echo $a['calibrarea_jos']?></h2>
-	 </div>
--->
-<!--
-<?php
-	if(isset($data['submit'])){
-?>	
-	<script>
-		alert("Calibrarea sus:<?php echo $a['calibrarea_sus']?>\n\nCalibrarea jos:<?php echo $a['calibrarea_jos']?>");	
-	</script>
-<?php	
-	}
-?>
--->
 </body>
 </html>
--- option.php
+++ option.php
@@ -3,18 +3,17 @@ require 'my_db.php';
 $query = "SELECT DISTINCT `Terminal` FROM `table_terminal`;";
 $sql = mysqli_query($mydb,$query );
 $a = mysqli_fetch_assoc($sql);
-//var_dump($a) ;
 $return= array();
 while($row = mysqli_fetch_assoc($sql)){
- $return[] = "<option value='".$row['Terminal']."'>".$row['Terminal']."</option>";
+	$return[] = $row['Terminal'];
 }
+asort($return);
 
 
 $query1 = "SELECT DISTINCT `nr_mini` FROM `terminal_proprietes`;";
 $sql1 = mysqli_query($mydb,$query1);
 $a1 = mysqli_fetch_assoc($sql1);
 $return1 = array();
-//var_dump($return1);
 while($row1 = mysqli_fetch_assoc($sql1)){
  	$return1[] = $row1['nr_mini'];
 }	
@@ -27,7 +26,6 @@ $query2 = "SELECT DISTINCT `nr_presetei` FROM `terminal_proprietes`;";
 $sql2 = mysqli_query($mydb,$query2);
 $a2 = mysqli_fetch_assoc($sql2);
 $return2 = array();
-//var_dump($return1);
 while($row2 = mysqli_fetch_assoc($sql2)){
  	$return2[] = $row2['nr_presetei'];
 }
@@ -39,11 +37,22 @@ $query3 = "SELECT DISTINCT `sez` FROM `terminal_proprietes`;";
 $sql3 = mysqli_query($mydb,$query3);
 $a3 = mysqli_fetch_assoc($sql3);
 $return3 = array();
-//var_dump($return1);
 while($row3 = mysqli_fetch_assoc($sql3)){
- 	$return3[] = $row3['sez'];
+	$return3[] = $row3['sez'];
 }
 asort($return3);
+
+
+$data_option = $_POST;
+$query = "SELECT tp.calibrarea_sus , tp.calibrarea_jos 
+         FROM `terminal_proprietes` tp INNER JOIN table_terminal tt ON tp.id_terminal = tt.id 
+		 WHERE tp.nr_presetei = '".$data_option['nr_presetei']."' 
+		 AND tp.nr_mini ='".$data_option['mini']."' 
+		 AND tp.sez = '".$data_option['sez']."' 
+		 AND tt.terminal = '".$data_option['terminal']."'" ;
+$calibrarea = mysqli_query($mydb,$query);
+$b = mysqli_fetch_assoc($calibrarea);
+var_dump($b);
 
 ?>
 <!DOCTYPE html>
@@ -60,16 +69,25 @@ asort($return3);
 			margin:auto;
 			text-align:center;
 		}
-		option{
-			margin-left:50%;
-		}
+	
+		
 	
 	</style>
+
+	<script>
+		function SlideDown (object){
+			$(object).css("dispaly","block");
+		};
+		function SlideUP (object){
+			$(object).slideUp;
+		}
+	</script>
+
 </head>
 <body>
-<form action="content.php" method="post">
+<form action="" method="post">
 <label for="terminal">Terminal</label>
-	<select name="terminal" id="" value="Terminal">
+	<select name="terminal" id="" value="terminal">
 	<option value="" selected disabled></option>
 		<?php
 			foreach($return as $resault){
@@ -107,5 +125,23 @@ asort($return3);
 	<button class="btn btn-primary " type="submit">Отправить</button>
 	
 </form>
+<?php
+	if($b){
+?>
+<div class="result">
+
+	<script>
+		SlideUP("result");
+	</script>
+
+	Calibrarea sus: <?=$b['calibrarea_sus']?>
+	Calibrarea sus: <?=$b['calibrarea_jos']?>
+</div>
+<?php
+	}else{
+?>
+<?php
+	}
+?>
 </body>
 </html>
